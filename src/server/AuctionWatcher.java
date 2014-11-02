c471c2a9fea11bbf95c8a3d78bc83c867e1eae04
Marco Jansen <jansen.marco95@gmail.com>
2014-11-02 18:34:00 +0100

    javadoc

--- src/server/AuctionWatcher.java
+++ src/server/AuctionWatcher.java
@@ -3,14 +3,27 @@ package server;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+/**
+ * Auctionwatcher
+ * Checks every second if an auction has expired.
+ */
 public class AuctionWatcher extends Thread {
 	
 	private Model model;
 
+	/**
+	 * Constructor to create an Auction Watcher
+	 * @param model		Model
+	 */
 	public AuctionWatcher(Model model) {
 		this.model = model;
 	}
 	
+	/**
+	 * Checks every second if an auction has ended. 
+	 * If an auction has ended it moves the auction from the active auction list to ended auction list.
+	 * Also sends winner to server with the auction id that was ended.
+	 */
 	@Override
 	public void run() {
 		while(true){
